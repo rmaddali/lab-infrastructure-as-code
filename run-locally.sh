f9af7eb919068ea5756fc846d36858c3461ede1c
Deven Phillips <deven.phillips@redhat.com>
2018-06-09 11:29:29 -0400

    Scripts for running automation

---
+++ run-locally.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+minishift addons enable anyuid
+minishift addons enable admin-user
+minishift addons enable xpaas
+minishift addons disable che
+minishift addons disable registry-route
+
+minishift config set network-nameserver 8.8.8.8
+
+minishift start --skip-registration --iso-url=centos --cpus=4 --memory=10GB --vm-driver=virtualbox
+
+oc login -u developer -p developer --insecure-skip-tls-verify=true https://$(minishift ip):8443/
+
+ansible-galaxy install -t requirements.yml --roles-path=roles
+
+ansible-playbook apply.yml -i inventory/ -e target=bootstrap
+
+ansible-playbook apply.yml -i inventory/ -e target=tools
+
+#ansible-playbook apply.yml -i inventory/ -e target=apps
---
+++ run-remote.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.dff8.openshift.opentlc.com/
