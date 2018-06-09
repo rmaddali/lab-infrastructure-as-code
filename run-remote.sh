bc2f78358350d67180713db961e1680c8b5d5711
Deven Phillips <deven.phillips@redhat.com>
2018-06-09 11:30:21 -0400

    Scripted Ansible for remote cluster

--- run-remote.sh
+++ run-remote.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.dff8.openshift.opentlc.com/
+
+ansible-galaxy install -t requirements.yml --roles-path=roles
+
+ansible-playbook apply.yml -i inventory/ -e target=bootstrap
+
+ansible-playbook apply.yml -i inventory/ -e target=tools
