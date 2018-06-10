2183c477e5693b8942c6958cbf57e679adb9a209
Deven Phillips <deven.phillips@gmail.com>
2018-06-10 00:51:31 -0400

    Reconfigured to use project name postfix

--- run-remote.sh
+++ run-remote.sh
@@ -4,6 +4,6 @@ oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.d
 
 ansible-galaxy install -r requirements.yml --roles-path=roles
 
-ansible-playbook apply.yml -i inventory/ -e target=bootstrap
+ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=user1" -e target=bootstrap
 
-ansible-playbook apply.yml -i inventory/ -e target=tools
+ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=user1" -e target=tools
