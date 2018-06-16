8042f342db55facf8cc02dbb1ee34a80f42ff69c
Deven Phillips <deven.phillips@gmail.com>
2018-06-16 12:33:18 -0400

    Reworked order and added unprocessed templates

--- run-remote.sh
+++ run-remote.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.dff8.openshift.opentlc.com/
+oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.qcon.openshift.opentlc.com/
 
 ansible-galaxy install -r requirements.yml --roles-path=roles
 
