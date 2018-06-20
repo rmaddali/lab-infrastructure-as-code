7b4d281617b216deb2593bcf809b0fc8af3ffa6c
Deven Phillips <deven.phillips@gmail.com>
2018-06-20 14:49:39 -0400

    Cleanup after adding rolebindings

--- run-remote.sh
+++ run-remote.sh
@@ -3,10 +3,12 @@
 set -x
 set -e
 
-oc login -u user1 -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.qcon.openshift.opentlc.com/
+export USERNAME=''
+
+oc login -u ${USERNAME} -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.qcon.openshift.opentlc.com/
 
 ansible-galaxy install -r requirements.yml --roles-path=roles
 
-ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=user1" -e target=bootstrap
+ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${USERNAME}" -e target=bootstrap
 
-ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=user1" -e target=tools
+ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${USERNAME}" -e target=tools
