4f6cd32ebab1221ba95077489dab735889e31be5
Deven Phillips <deven.phillips@redhat.com>
2018-06-21 13:45:45 -0400

    Fixed script cleanup

--- run-locally.sh
+++ run-locally.sh
@@ -4,8 +4,8 @@ set -x
 set -e
 
 ## Remove any user-specific settings from the inventory
-egrep -r "user[0-9]*" * | awk -F":" '{print $1}' | sort | uniq | grep -v openshift-template | xargs -n 1 sed -i 's@ (user[0-9]*)$@@g'
-egrep -r "user[0-9]*" * | awk -F":" '{print $1}' | sort | uniq | grep -v openshift-template | xargs -n 1 sed -i 's@user[0-9]*$@@g'
+egrep -r "user[0-9]*" params/* | awk -F":" '{print $1}' | sort | uniq | grep -v openshift-template | xargs -n 1 sed -i 's@ (user[0-9]*)$@@g'
+egrep -r "user[0-9]*" params/* | awk -F":" '{print $1}' | sort | uniq | grep -v openshift-template | xargs -n 1 sed -i 's@user[0-9]*$@@g'
 
 minishift addons enable anyuid
 minishift addons enable admin-
