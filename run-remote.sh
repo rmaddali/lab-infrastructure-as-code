aa83e6ee7a16d2d9a456a68bdd230fb0d62ba3d8
Deven Phillips <deven.phillips@redhat.com>
2018-06-21 14:34:41 -0400

    Prevent run when username not set

--- run-remote.sh
+++ run-remote.sh
@@ -3,18 +3,26 @@
 set -x
 set -e
 
-export USERNAME='user10'
+if [[ "${OPENSHIFT_USER}X" == "X" ]]; then
+    printf "Enter your OpenShift user ID: "
+    read username
+    echo ${username} | grep "^user[0-9]*" && export OPENSHIFT_USERNAME="${username}" || echo "Invalid username"
+fi
 
-oc login -u ${USERNAME} -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.qcon.openshift.opentlc.com/
+if [[ "${OPENSHIFT_USERNAME}X" != "X" ]]; then
 
-DOCKER_COMMAND="docker run -v $HOME/.kube/config:/openshift-applier/.kube/config:z -w /tmp/my-inventory -u root -v $PWD:/tmp/my-inventory -e INVENTORY_PATH=/tmp/my-inventory -t redhatcop/openshift-applier"
+    oc login -u ${OPENSHIFT_USERNAME} -p "r3dh4t1!" --insecure-skip-tls-verify=true https://master.qcon.openshift.opentlc.com/
 
-${DOCKER_COMMAND} ./clean-unique-names.sh
+    DOCKER_COMMAND="docker run -v $HOME/.kube/config:/openshift-applier/.kube/config:z -w /tmp/my-inventory -u root -v $PWD:/tmp/my-inventory -e INVENTORY_PATH=/tmp/my-inventory -t redhatcop/openshift-applier"
 
-${DOCKER_COMMAND} ansible-galaxy install -r requirements.yml --roles-path=roles
+    ${DOCKER_COMMAND} ./clean-unique-names.sh
 
-${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${USERNAME}" -e target=bootstrap
+    ${DOCKER_COMMAND} ansible-galaxy install -r requirements.yml --roles-path=roles
 
-${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${USERNAME}" -e target=tools
+    ${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${OPENSHIFT_USERNAME}" -e target=bootstrap
 
-${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${USERNAME}" -e target=apps
+    ${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${OPENSHIFT_USERNAME}" -e target=tools
+
+    ${DOCKER_COMMAND} ansible-playbook unique-projects-playbook.yaml -i inventory/ -e "project_name_postfix=${OPENSHIFT_USERNAME}" -e target=apps
+
+fi
